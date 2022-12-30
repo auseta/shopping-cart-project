526cd6ba165043fe30643bf9723adaacb86d140f
Augusto Seta <alseta97@gmail.com>
2022-12-29 23:55:49 -0300

    the react router library was imported.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './styles/style.css';
 import Header from './components/Header';
+import Home from './components/Home';
+import Shop from './components/Shop';
 
 function App() {
   const [ cart, setCart ] = useState(0)
@@ -12,6 +15,12 @@ function App() {
   return (
     <div className="App">
       <Header cart={ cart } events={ incrementCart } />
+      <Router>
+        <Routes>
+          <Route path='/' element={ <Home /> } />
+          <Route path='/shop' element={ <Shop /> } />
+        </Routes>
+      </Router>
     </div>
   );
 }
