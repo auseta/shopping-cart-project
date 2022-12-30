ee752ad8c36125fbb5bc149cd609e005821d35a1
Augusto Seta <alseta97@gmail.com>
2022-12-29 23:53:12 -0300

    the Shop component was created.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,40 @@
-import React from 'react';
-import logo from '../images/logo.png'
+import React from "react";
+import logo from "../images/logo.png";
 
 const Header = ({ cart, events }) => {
+
   return (
-    <div className='header'>
-      <div className='moving-header'>
+    <div className="header">
+      <div className="moving-header">
         <h4>FREE SHIPPING OVER $500.00</h4>
         <h4>UP TO 6 INSTALLMENTS WITHOUT INTEREST ON CLOTHES</h4>
       </div>
-      <div className='main-header'>
-        <div className='header-name'>
-          <img className='logo' src={ logo } />
-          <h1>SETA<b>GAMING</b></h1>
+      <div className="main-header">
+        <div className="header-name">
+          <img className="logo" src={logo} alt="logo" />
+          <h1>
+            SETA<b>GAMING</b>
+          </h1>
         </div>
-        <i class="fa-sharp fa-solid fa-cart-shopping" onClick={ events } >
-          { cart === 0 ? null : <span>{ cart }</span> }
+        <i className="fa-sharp fa-solid fa-cart-shopping" onClick={events}>
+          {cart === 0 ? null : <span>{cart}</span>}
         </i>
       </div>
-      <div className='main-navbar'>
+      <div className="main-navbar">
         <ul>
-          <li><i class="fa-sharp fa-solid fa-bag-shopping"></i>Shop</li>
-          <li><i class="fa-solid fa-suitcase"></i>About Us</li>
-          <li><i class="fa-solid fa-message"></i>Contact Us</li>
+          <li>
+            <i className="fa-sharp fa-solid fa-bag-shopping"></i>Shop
+          </li>
+          <li>
+            <i className="fa-solid fa-suitcase"></i>About Us
+          </li>
+          <li>
+            <i className="fa-solid fa-message"></i>Contact Us
+          </li>
         </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
