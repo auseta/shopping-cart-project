e76891a91df8d072ed9d868a1b3d45d0a9dae7f2
Augusto Seta <alseta97@gmail.com>
2022-12-31 14:56:41 -0300

    Footer component created.

---
+++ src/components/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../images/logo.png'
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <div className='social-media-links'>
+        <i className="fa-brands fa-instagram"></i>
+        <i className="fa-brands fa-facebook"></i>
+        <i className="fa-brands fa-twitter"></i>
+        <i className="fa-brands fa-linkedin"></i>
+      </div>
+      <h4>Project made it by Augusto Seta</h4>
+    </div>
+  )
+}
+
+export default Footer;
