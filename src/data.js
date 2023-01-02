caa2507a66e26fdcef613811663a1cce0b80c8a1
Augusto Seta <alseta97@gmail.com>
2023-01-02 00:56:38 -0300

    added more items information to the data.js file

---
+++ src/data.js
@@ -0,0 +1,65 @@
+import item1 from './images/item1.jpg';
+import item2 from './images/item2.jpg';
+import item3 from './images/item3.jpg';
+import item4 from './images/item4.jpg';
+import item5 from './images/item5.jpg';
+import item6 from './images/item6.jpg';
+import item7 from './images/item7.jpg';
+import item8 from './images/item8.jpg';
+import item9 from './images/item9.jpg';
+import item10 from './images/item10.jpg';
+
+const components = [
+  {
+    name: 'Micro Intel I3-10100F 4.3Ghz 6Mb S.1200',
+    img: item3,
+    type: 'Processor'
+  },
+  {
+    name: 'Micro Amd Ryzen 5 5600g 4.4 Ghz Am4',
+    img: item2,
+    type: 'Processor'
+  },
+  {
+    name: 'Amd Radeon Powercolor rx 6600',
+    img: item1,
+    type: 'videoCard'
+  },
+  {
+    name: 'Micro Intel Celeron G5900 3.4ghz 3mb s.1200',
+    img: item4,
+    type: 'Processor'
+  },
+  {
+    name: 'Micro AMD Ryzen 5 3600 4.2 Ghz AM4',
+    img: item5,
+    type: 'Processor'
+  },
+  {
+    name: 'RAM PNY XLR8 Gaming EPIC-X Silver 8GB 3600Mhz RGB DDR4',
+    img: item6,
+    type: 'RAM'
+  },
+  {
+    name: 'RAM PNY XLR8 GAMING 8GB 3200Mhz RGB DDR4',
+    img: item7,
+    type: 'RAM'
+  },
+  {
+    name: 'PNY Nvidia Geforce GTX 1660 Ti Dual Fan 6GB GDDR6',
+    img: item8,
+    type: 'videoCard'
+  },
+  {
+    name: 'Mother Asus Prime A320m-k Am4 Ddr4 A320 Hdmi M2 Pcreg',
+    img: item9,
+    type: 'Mother'
+  },
+  {
+    name: 'Nvidia MSI GeForce 700 Series GT 730 N730K-2GD3H/LPV1 2GB',
+    img: item10,
+    type: 'videoCard'
+  }
+]
+
+export default components
