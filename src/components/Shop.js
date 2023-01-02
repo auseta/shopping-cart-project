ed2523ea7b594ab75b1429f4c9cf85d20c0e287b
Augusto Seta <alseta97@gmail.com>
2023-01-02 00:54:36 -0300

    small modifications in the Shop component

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from "react";
+import components from "../data";
 
 const Shop = () => {
+  console.log(components);
   return (
-    <div className='shop'>
-      <h1>Shop</h1>
+    <div className="shop">
+      {components.map((component, index) => (
+        <div className="item" key={ index }>
+          <img src={ component.img } alt="product" />
+          <h4>{component.name}</h4>
+          <h4 className="item-price">$500</h4>
+          <div className="item-buttons">
+            <button className="amount-button" >-</button>
+            <span>0</span>
+            <button className="amount-button" >+</button>
+          </div>
+          <button className="add-button">Add to cart</button>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
 export default Shop;
