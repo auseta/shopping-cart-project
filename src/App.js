1bb4f1b50f3568d3bbe19a27609b4177da285516
Augusto Seta <alseta97@gmail.com>
2023-01-05 00:16:38 -0300

    A state is declared that contains the information of all the products. Two functions were created to update the number of units that we want to buy of a certain product.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './styles/style.css';
+import DATA from './data';
 import Header from './components/Header';
 import Home from './components/Home';
 import Shop from './components/Shop';
 import Footer from './components/Footer';
 
-function App() {
-  const [ cart, setCart ] = useState(0)
 
-  const incrementCart = () => {
-    setCart( cart + 1 )
+function App() {
+
+  const [products, setProducts] = useState(DATA)
+
+  const incrementProductAmount = (productName) => {
+    const UPDATED_PRODUCTS_AMOUNT = products.map( product => {
+      if ( product.name === productName ) {
+        product.amount = product.amount + 1
+      }
+      return product
+    })
+    setProducts( UPDATED_PRODUCTS_AMOUNT )
+  }
+
+  const decrementProductAmount = (productName) => {
+    const UPDATED_PRODUCTS_AMOUNT = products.map( product => {
+      if ( product.name === productName && product.amount !== 0 ) {
+        product.amount = product.amount - 1
+      }
+      return product
+    })
+    setProducts( UPDATED_PRODUCTS_AMOUNT )
+  }
+
+  const ACTIONS = {
+    increment : incrementProductAmount,
+    decrement : decrementProductAmount
   }
 
   return (
     <Router>
       <div className="App">
-        <Header cart={ cart } events={ incrementCart } />
+        <Header />
           <Routes>
             <Route path='/' element={ <Home /> } />
-            <Route path='/shop' element={ <Shop /> } />
+            <Route path='/shop' element={ <Shop products={ products } actions={ ACTIONS } /> }/>
           </Routes>
         <Footer />
       </div>
