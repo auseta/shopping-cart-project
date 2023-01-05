267ebb9e3d288f83f86014bbae31d8d556558dd3
Augusto Seta <alseta97@gmail.com>
2023-01-05 00:18:36 -0300

    minor changes were made. variable name changed; added a quantity key that has the value of the number of units of an item to buy.

--- src/data.js
+++ src/data.js
@@ -8,58 +8,82 @@ import item7 from './images/item7.jpg';
 import item8 from './images/item8.jpg';
 import item9 from './images/item9.jpg';
 import item10 from './images/item10.jpg';
+import item11 from './images/item11.jpg';
+import item12 from './images/item12.jpg';
 
-const components = [
+const DATA = [
   {
     name: 'Micro Intel I3-10100F 4.3Ghz 6Mb S.1200',
     img: item3,
-    type: 'Processor'
+    type: 'Processor',
+    amount: 0
   },
   {
     name: 'Micro Amd Ryzen 5 5600g 4.4 Ghz Am4',
     img: item2,
-    type: 'Processor'
+    type: 'Processor',
+    amount: 0
   },
   {
     name: 'Amd Radeon Powercolor rx 6600',
     img: item1,
-    type: 'videoCard'
+    type: 'videoCard',
+    amount: 0
   },
   {
     name: 'Micro Intel Celeron G5900 3.4ghz 3mb s.1200',
     img: item4,
-    type: 'Processor'
+    type: 'Processor',
+    amount: 0
   },
   {
     name: 'Micro AMD Ryzen 5 3600 4.2 Ghz AM4',
     img: item5,
-    type: 'Processor'
+    type: 'Processor',
+    amount: 0
   },
   {
     name: 'RAM PNY XLR8 Gaming EPIC-X Silver 8GB 3600Mhz RGB DDR4',
     img: item6,
-    type: 'RAM'
+    type: 'RAM',
+    amount: 0
   },
   {
     name: 'RAM PNY XLR8 GAMING 8GB 3200Mhz RGB DDR4',
     img: item7,
-    type: 'RAM'
+    type: 'RAM',
+    amount: 0
   },
   {
     name: 'PNY Nvidia Geforce GTX 1660 Ti Dual Fan 6GB GDDR6',
     img: item8,
-    type: 'videoCard'
+    type: 'videoCard',
+    amount: 0
   },
   {
     name: 'Mother Asus Prime A320m-k Am4 Ddr4 A320 Hdmi M2 Pcreg',
     img: item9,
-    type: 'Mother'
+    type: 'Mother',
+    amount: 0
   },
   {
     name: 'Nvidia MSI GeForce 700 Series GT 730 N730K-2GD3H/LPV1 2GB',
     img: item10,
-    type: 'videoCard'
+    type: 'videoCard',
+    amount: 0
+  },
+  {
+    name: 'Motherboard Gigabyte A320M-H AM4',
+    img : item11,
+    type: 'Mother',
+    amount: 0
+  },
+  {
+    name: 'Motherboard Msi A520m Pro vh Am4',
+    img: item12,
+    type: 'Mother',
+    amount: 0
   }
-]
+];
 
-export default components
+export default DATA;
