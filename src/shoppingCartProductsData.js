ed05ab0247f6140101effadffa8c422d47746482
Augusto Seta <alseta97@gmail.com>
2023-01-09 15:25:27 -0300

    created file containing an object representing products containing a non-0 quantity in the ShoppingCart.js component.

---
+++ src/shoppingCartProductsData.js
@@ -0,0 +1,52 @@
+const shoppingCartProductsData = [
+  {
+    name: 'Micro Intel I3-10100F 4.3Ghz 6Mb S.1200',
+    amount: 0
+  },
+  {
+    name: 'Micro Amd Ryzen 5 5600g 4.4 Ghz Am4',
+    amount: 0
+  },
+  {
+    name: 'Amd Radeon Powercolor rx 6600',
+    amount: 0
+  },
+  {
+    name: 'Micro Intel Celeron G5900 3.4ghz 3mb s.1200',
+    amount: 0
+  },
+  {
+    name: 'Micro AMD Ryzen 5 3600 4.2 Ghz AM4',
+    amount: 0
+  },
+  {
+    name: 'RAM PNY XLR8 Gaming EPIC-X Silver 8GB 3600Mhz RGB DDR4',
+    amount: 0
+  },
+  {
+    name: 'RAM PNY XLR8 GAMING 8GB 3200Mhz RGB DDR4',
+    amount: 0
+  },
+  {
+    name: 'PNY Nvidia Geforce GTX 1660 Ti Dual Fan 6GB GDDR6',
+    amount: 0
+  },
+  {
+    name: 'Mother Asus Prime A320m-k Am4 Ddr4 A320 Hdmi M2 Pcreg',
+    amount: 0
+  },
+  {
+    name: 'Nvidia MSI GeForce 700 Series GT 730 N730K-2GD3H/LPV1 2GB',
+    amount: 0
+  },
+  {
+    name: 'Motherboard Gigabyte A320M-H AM4',
+    amount: 0
+  },
+  {
+    name: 'Motherboard Msi A520m Pro vh Am4',
+    amount: 0
+  }
+]
+
+export default shoppingCartProductsData;
