4abf2f94fb5c4105ad70c99714af551b05470f79
Augusto Seta <alseta97@gmail.com>
2023-01-10 14:04:28 -0300

    state created to render the number of products in the cart logo.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './styles/style.css';
-import DATA from './data';
-import shoppingCartProductsData from './shoppingCartProductsData.js';
+import DATA from './data/data';
+import shoppingCartProductsData from './data/shoppingCartProductsData';
 import Header from './components/Header';
 import Home from './components/Home';
 import Shop from './components/Shop';
@@ -12,8 +12,18 @@ import ShoppingCart from './components/ShoppingCart';
 
 function App() {
 
-  const [ productsInCart, setProductsInCart ] = useState(shoppingCartProductsData)
-  const [ products, setProducts ] = useState(DATA)
+  const [ productsInCart, setProductsInCart ] = useState(shoppingCartProductsData);
+  const [ cartProductsAmount, setCartProductsAmount ] = useState(0);
+  const [ products, setProducts ] = useState(DATA);
+
+  useEffect( () => {
+
+    let amount = 0;
+    for (const product of productsInCart) {
+      amount += product.amount
+    }
+    setCartProductsAmount(amount)
+  }, [productsInCart])
 
   const incrementProductAmount = (productName) => {
     const UPDATED_PRODUCTS_AMOUNT = products.map( product => {
@@ -36,28 +46,50 @@ function App() {
   }
 
   const addProductToCart = (productName) => {
+    const product = products.find(product => product.name === productName);
+    const UPDATED_PRODUCTS_AMOUNT = [...productsInCart];
+    UPDATED_PRODUCTS_AMOUNT.map( productInCart => {
+      if ( productInCart.name === productName && productInCart.amount !== 0 ) {
+        productInCart.amount += product.amount;
+        productInCart.price += product.price * product.amount;
+        product.amount = 0;
+      } else if ( productInCart.name === productName && productInCart.amount === 0 ) {
+        productInCart.amount = product.amount;
+        productInCart.price = product.price * product.amount;
+        product.amount = 0;
+      }
+      return true
+    })
+    setProductsInCart(UPDATED_PRODUCTS_AMOUNT);
+  }
+
+  const removeProductFromCart = (productName) => {
     const product = products.find(element => element.name === productName);
     const UPDATED_PRODUCTS_AMOUNT = [...productsInCart];
     UPDATED_PRODUCTS_AMOUNT.map( productInCart =>(
-      productInCart.name === productName ? productInCart.amount = product.amount : null  
+      productInCart.name === productName ? ( 
+        productInCart.amount = 0,
+        productInCart.price = product.price
+      ) : null
     ))
     setProductsInCart(UPDATED_PRODUCTS_AMOUNT)
   }
 
   const ACTIONS = {
-    increment : incrementProductAmount,
-    decrement : decrementProductAmount,
-    add : addProductToCart,
+    increment: incrementProductAmount,
+    decrement: decrementProductAmount,
+    add: addProductToCart,
+    remove: removeProductFromCart
   }
 
   return (
     <Router>
       <div className="App">
-        <Header />
+        <Header amount={ cartProductsAmount } />
           <Routes>
             <Route path='/' element={ <Home /> } />
             <Route path='/shop' element={ <Shop products={ products } actions={ ACTIONS } /> }/>
-            <Route path='/shopping-cart' element={ <ShoppingCart products={ productsInCart } /> } />
+            <Route path='/shopping-cart' element={ <ShoppingCart products={ productsInCart } remove={ ACTIONS.remove } /> } />
           </Routes>
         <Footer />
       </div>
