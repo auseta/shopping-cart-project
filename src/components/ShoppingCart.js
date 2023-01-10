3d3c919e270d656a1b1d2566101eaa5e07ff4c29
Augusto Seta <alseta97@gmail.com>
2023-01-10 14:12:45 -0300

    ShoppingCart shows a list of all the products added to the cart as well as their quantities, prices and at the end the total amount to pay

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const ShoppingCart = ({ products }) => {
+const ShoppingCart = ({ products, remove }) => {
 
-  console.log(products);
+  const [ totalPrice, setTotalPrice ] = useState(0);
+
+  useEffect( () => {
+    let updatedTotalPrice = 0
+
+    for (const product of products) {
+      if (product.amount !== 0) {
+        updatedTotalPrice += product.price
+      }
+    }
+  
+    setTotalPrice(updatedTotalPrice);
+  },[products])
 
   return(
     <div className='shopping-cart'>
-      <h1>Shopping Cart</h1>
+      <h2>Your products</h2>
       {
-        products.map( (product,index) => {
-          if ( product.amount !== 0 ) {
-            return (
-              <div className='shopping-cart-product' key={ index }>
-                <h6>{ product.name } x { product.amount }</h6>
-              </div>
-            )
-          }
-        })
+        products.map( (product,index) => (
+          product.amount === 0 ? null : (
+            <div className='shopping-cart-product' key={ index }>
+                <h4>{ product.name } x { product.amount } - ${ product.price }</h4><i onClick={ ()=> remove(product.name) } className="fas fa-times-circle"></i>
+            </div>
+          )
+        ))
       }
+      <div className='shopping-cart-total-price'> Price to pay: ${ totalPrice }</div>
     </div>
   )
 }
