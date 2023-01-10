f537f07b4e75ba8aad78443e1d2fd9cd85d5036a
Augusto Seta <alseta97@gmail.com>
2023-01-10 14:14:07 -0300

    Created data folder which contains the data of the components to be rendered in the Shop component and the data to be updated by the user.

---
+++ src/data/data.js
@@ -0,0 +1,101 @@
+import item1 from '../images/item1.jpg'
+import item2 from '../images/item2.jpg';
+import item3 from '../images/item3.jpg';
+import item4 from '../images/item4.jpg';
+import item5 from '../images/item5.jpg';
+import item6 from '../images/item6.jpg';
+import item7 from '../images/item7.jpg';
+import item8 from '../images/item8.jpg';
+import item9 from '../images/item9.jpg';
+import item10 from '../images/item10.jpg';
+import item11 from '../images/item11.jpg';
+import item12 from '../images/item12.jpg';
+
+const DATA = [
+  {
+    name: 'Micro Intel I3-10100F 4.3Ghz 6Mb S.1200',
+    img: item3,
+    type: 'Processor',
+    price: 79,
+    amount: 0,
+  },
+  {
+    name: 'Micro Amd Ryzen 5 5600g 4.4 Ghz Am4',
+    img: item2,
+    type: 'Processor',
+    price: 129,
+    amount: 0
+  },
+  {
+    name: 'Amd Radeon Powercolor rx 6600',
+    img: item1,
+    type: 'videoCard',
+    price: 270,
+    amount: 0
+  },
+  {
+    name: 'Micro Intel Celeron G5900 3.4ghz 3mb s.1200',
+    img: item4,
+    type: 'Processor',
+    price: 50,
+    amount: 0
+  },
+  {
+    name: 'Micro AMD Ryzen 5 3600 4.2 Ghz AM4',
+    img: item5,
+    type: 'Processor',
+    price: 116,
+    amount: 0
+  },
+  {
+    name: 'RAM PNY XLR8 Gaming EPIC-X Silver 8GB 3600Mhz RGB DDR4',
+    img: item6,
+    type: 'RAM',
+    price: 45,
+    amount: 0
+  },
+  {
+    name: 'RAM PNY XLR8 GAMING 8GB 3200Mhz RGB DDR4',
+    img: item7,
+    type: 'RAM',
+    price: 37,
+    amount: 0
+  },
+  {
+    name: 'PNY Nvidia Geforce GTX 1660 Ti Dual Fan 6GB GDDR6',
+    img: item8,
+    type: 'videoCard',
+    price: 580,
+    amount: 0
+  },
+  {
+    name: 'Mother Asus Prime A320m-k Am4 Ddr4 A320 Hdmi M2 Pcreg',
+    img: item9,
+    type: 'Mother',
+    price: 200,
+    amount: 0
+  },
+  {
+    name: 'Nvidia MSI GeForce 700 Series GT 730 N730K-2GD3H/LPV1 2GB',
+    img: item10,
+    type: 'videoCard',
+    price: 450,
+    amount: 0
+  },
+  {
+    name: 'Motherboard Gigabyte A320M-H AM4',
+    img : item11,
+    type: 'Mother',
+    price: 170,
+    amount: 0
+  },
+  {
+    name: 'Motherboard Msi A520m Pro vh Am4',
+    img: item12,
+    type: 'Mother',
+    price: 150,
+    amount: 0
+  }
+];
+
+export default DATA;
---
+++ src/data/shoppingCartProductsData.js
@@ -0,0 +1,64 @@
+const shoppingCartProductsData = [
+  {
+    name: 'Micro Intel I3-10100F 4.3Ghz 6Mb S.1200',
+    price: 79,
+    amount: 0
+  },
+  {
+    name: 'Micro Amd Ryzen 5 5600g 4.4 Ghz Am4',
+    price: 129,
+    amount: 0
+  },
+  {
+    name: 'Amd Radeon Powercolor rx 6600',
+    price: 270,
+    amount: 0
+  },
+  {
+    name: 'Micro Intel Celeron G5900 3.4ghz 3mb s.1200',
+    price: 50,
+    amount: 0
+  },
+  {
+    name: 'Micro AMD Ryzen 5 3600 4.2 Ghz AM4',
+    price: 116,
+    amount: 0
+  },
+  {
+    name: 'RAM PNY XLR8 Gaming EPIC-X Silver 8GB 3600Mhz RGB DDR4',
+    price: 45,
+    amount: 0
+  },
+  {
+    name: 'RAM PNY XLR8 GAMING 8GB 3200Mhz RGB DDR4',
+    price: 37,
+    amount: 0
+  },
+  {
+    name: 'PNY Nvidia Geforce GTX 1660 Ti Dual Fan 6GB GDDR6',
+    price: 580,
+    amount: 0
+  },
+  {
+    name: 'Mother Asus Prime A320m-k Am4 Ddr4 A320 Hdmi M2 Pcreg',
+    price: 200,
+    amount: 0
+  },
+  {
+    name: 'Nvidia MSI GeForce 700 Series GT 730 N730K-2GD3H/LPV1 2GB',
+    price: 450,
+    amount: 0
+  },
+  {
+    name: 'Motherboard Gigabyte A320M-H AM4',
+    price: 170,
+    amount: 0
+  },
+  {
+    name: 'Motherboard Msi A520m Pro vh Am4',
+    price: 150,
+    amount: 0
+  }
+]
+
+export default shoppingCartProductsData;
