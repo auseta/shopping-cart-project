e9aefc183f897b5ffdd5bac693380ee083666475
Augusto Seta <alseta97@gmail.com>
2023-01-10 14:10:29 -0300

    Shop allows the user to buy products for x units, accumulating the price by quantity and setting the counter to 0 once said product has been added to the cart.

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -8,13 +8,13 @@ const Shop = ({ products, actions }) => {
         <div className="item" key={ index }>
           <img src={ product.img } alt="product" />
           <h4>{product.name}</h4>
-          <h4 className="item-price">$500</h4>
+          <h4 className="item-price">${product.price}</h4>
           <div className="item-buttons">
             <button className="amount-button" onClick={ () => actions.decrement(product.name) } >-</button>
             <span>{ product.amount }</span>
             <button className="amount-button" onClick={ () => actions.increment(product.name) } >+</button>
           </div>
-          <button className="add-button" onClick={ () => actions.add(product.name) } >Add to cart</button>
+          <button className="add-button" onClick={ product.amount !== 0 ? () => actions.add(product.name) : null } >Add to cart</button>
         </div>
       ))}
     </div>
