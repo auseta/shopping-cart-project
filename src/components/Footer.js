e752a99f73a2d772d50919b580c63cc6b03eb679
Augusto Seta <alseta97@gmail.com>
2023-01-12 22:08:01 -0300

    class names changed to follow BEM methodologies.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,13 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className='footer'>
-      <div className='social-media-links'>
-        <i className="fa-brands fa-instagram"></i>
-        <i className="fa-brands fa-facebook"></i>
-        <i className="fa-brands fa-twitter"></i>
-        <i className="fa-brands fa-linkedin"></i>
+      <div className='footer__links'>
+        <i className="footer__i i fa-brands fa-instagram"></i>
+        <i className="footer__i i fa-brands fa-facebook"></i>
+        <i className="footer__i i fa-brands fa-twitter"></i>
+        <i className="footer__i i fa-brands fa-linkedin"></i>
       </div>
-      <h4>Project made it by Augusto Seta</h4>
+      <h4 className='footer__h4' >Project made it by Augusto Seta</h4>
     </div>
   )
 }
