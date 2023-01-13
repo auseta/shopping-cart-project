819d662733e9a28ff871c21b56174071baa1d979
Augusto Seta <alseta97@gmail.com>
2023-01-12 22:08:24 -0300

    class names changed to follow BEM methodologies.

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,38 +11,37 @@ const Header = ({ amount }) => {
 
   return (
     <div className="header">
-      <div className="moving-header">
-        <h4>FREE SHIPPING OVER $500.00</h4>
-        <h4>UP TO 6 INSTALLMENTS WITHOUT INTEREST ON CLOTHES</h4>
+      <div className="header__advertisement">
+        <h4 className="header__h4" >FREE SHIPPING OVER $500.00</h4>
+        <h4 className="header__h4" >UP TO 6 INSTALLMENTS WITHOUT INTEREST ON CLOTHES</h4>
       </div>
-      <div className="main-header">
-        <div className="header-name">
-          <img className="logo" src={logo} alt="logo" />
+      <div className="header__main">
+        <div className="header__title">
+          <img className="header__img" src={logo} alt="logo" />
           <Link to='/' style={ linkStyles } >
-            <h1>
+            <h1 className="header__h1">
               SETA<b>GAMING</b>
             </h1>
           </Link>
         </div>
         <Link to="/shopping-cart" style={ linkStyles } >
-          <i className="fa-sharp fa-solid fa-cart-shopping">
-            <span className={ amount !== 0 ? "cart-amount" : "" }>{ amount !== 0 ? amount : null }</span>
+          <i className="header__i fa-sharp fa-solid fa-cart-shopping">
+            <span className={ amount !== 0 ? "header__span" : "" }>{ amount !== 0 ? amount : null }</span>
           </i>
         </Link>
       </div>
-      <div className="main-navbar">
-        <ul>
-          
-            <li>
-              <Link to='/shop' style={ linkStyles } >
-                <i className="fa-sharp fa-solid fa-bag-shopping"></i>Shop
-              </Link>
-            </li>
-          <li>
-            <i className="fa-solid fa-suitcase"></i>About Us
+      <div className="header__nav">
+        <ul className="header__ul" >
+          <li className="header__li">
+            <Link to='/shop' style={ linkStyles } >
+              <i className="header__i fa-sharp fa-solid fa-bag-shopping"></i>Shop
+            </Link>
           </li>
-          <li>
-            <i className="fa-solid fa-message"></i>Contact Us
+          <li className="header__li">
+            <i className="header__i fa-solid fa-suitcase"></i>About Us
+          </li>
+          <li className="header__li">
+            <i className="header__i fa-solid fa-message"></i>Contact Us
           </li>
         </ul>
       </div>
