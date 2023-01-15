e166c470658cf330bd390b501f1fc378836c04b6
Augusto Seta <alseta97@gmail.com>
2023-01-14 21:42:49 -0300

    Added a hamburguer menu for the mobile version

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../images/logo.png";
 import { Link } from 'react-router-dom'
-
+import MobileList from './MobileList';
 const Header = ({ amount }) => {
+
+  const [ mobileState, setMobileState ] = useState({
+    active: false,
+  })
 
   const linkStyles = {
     textDecoration : 'none',
     color : 'inherit'
+  }
+
+  const setMobileMenuState = (classes) => {
+    setMobileState({active: !mobileState.active})
+    mobileState.active ? classes.toggle('open') : classes.toggle('open');
+  }
+
+  const handleMobileMenu = (e) => {
+    const $MOBILE__LIST = e.currentTarget;
+    const mobileClasses = $MOBILE__LIST.classList
+    setMobileMenuState(mobileClasses)
+  }
+
+  const handleMenuDisplay = () => {
+    if (mobileState.active) {
+      return 'enabled'
+    } else {
+      return 'disabled'
+    }
   }
 
   return (
@@ -18,32 +41,38 @@ const Header = ({ amount }) => {
       <div className="header__main">
         <div className="header__title">
           <img className="header__img" src={logo} alt="logo" />
-          <Link to='/' style={ linkStyles } >
-            <h1 className="header__h1">
-              SETA<b>GAMING</b>
-            </h1>
-          </Link>
+          <h1 className="header__h1">
+            SETA<b>GAMING</b>
+          </h1>
         </div>
-        <Link to="/shopping-cart" style={ linkStyles } >
-          <i className="header__i fa-sharp fa-solid fa-cart-shopping">
-            <span className={ amount !== 0 ? "header__span" : "" }>{ amount !== 0 ? amount : null }</span>
-          </i>
-        </Link>
-      </div>
-      <div className="header__nav">
-        <ul className="header__ul" >
-          <li className="header__li">
-            <Link to='/shop' style={ linkStyles } >
-              <i className="header__i fa-sharp fa-solid fa-bag-shopping"></i>Shop
+        <div className="header__nav">
+          <ul className="header__ul">
+            <Link to='/' style={ linkStyles } >
+              <li className="header__li">
+                <i className="header__i fa-solid fa-message"></i>Home
+              </li>
             </Link>
-          </li>
-          <li className="header__li">
-            <i className="header__i fa-solid fa-suitcase"></i>About Us
-          </li>
-          <li className="header__li">
-            <i className="header__i fa-solid fa-message"></i>Contact Us
-          </li>
-        </ul>
+            <Link to='/shop' style={ linkStyles } >
+              <li className="header__li" >
+                <i className="header__i fa-sharp fa-solid fa-bag-shopping"></i>Shop
+              </li>
+            </Link>
+            <Link to="/shopping-cart" style={ linkStyles } >
+              <li className="header__li" style={{ border: 'none' }} >
+                <i className="header__i fa-sharp fa-solid fa-cart-shopping">
+                  <span className={ amount !== 0 ? "header__span" : "" }>{ amount !== 0 ? amount : null }</span>
+                </i>
+              </li>
+            </Link>
+          </ul>
+        </div>
+        <div className="header__mobile" onClick={ (e) => handleMobileMenu(e) } >
+          <span className="header__span--mobile" ></span>
+          <span className="header__span--mobile" ></span>
+          <span className="header__span--mobile" ></span>
+          <span className="header__span--mobile" ></span>
+          <MobileList state={ handleMenuDisplay } />
+        </div>
       </div>
     </div>
   );
