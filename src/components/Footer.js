30987fa917eebd6f47cf0892e0d29fafe70c9cb3
Augusto Seta <alseta97@gmail.com>
2023-01-16 00:28:15 -0300

    Surrounded the LinkedIn icon in the Footer component with an anchor tag that redirects the user to the owner's Linkedin.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
+
   return (
     <div className='footer'>
       <div className='footer__links'>
         <i className="footer__i i fa-brands fa-instagram"></i>
         <i className="footer__i i fa-brands fa-facebook"></i>
-        <i className="footer__i i fa-brands fa-twitter"></i>
-        <i className="footer__i i fa-brands fa-linkedin"></i>
+        <i className="footer__i i fa-brands fa-twitter"></i> 
+        <a className='footer__a' href='https://www.linkedin.com/in/augusto-seta/'>
+          <i className="footer__i i fa-brands fa-linkedin"></i>
+        </a>
       </div>
       <h4 className='footer__h4' >Project made it by Augusto Seta</h4>
     </div>
